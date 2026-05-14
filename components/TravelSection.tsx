15c891793651485bdbb2d2562de28b9fc46262b6
Sam Echikson <sam@speccheckrx.com>
2026-05-14 12:20:24 -0700

    Add a couple travel section points

--- components/TravelSection.tsx
+++ components/TravelSection.tsx
@@ -1,81 +1,99 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
-import { ExternalLink } from 'lucide-react';
+import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { ExternalLink } from "lucide-react";
 
 const THINGS_TO_DO = [
   {
-    title: 'Food and Wine Tour',
-    description: "Walk the old city center and taste cheese, Chouriço (Portuguese sausage), bifana sandwiches, traditional rices, Port, and Lisbon wines.",
-    icon: '/images/travel-icons/food-tour.png',
-    link: 'https://tinyurl.com/yc6cs2c2'
+    title: "Food and Wine Tour",
+    description:
+      "Walk the old city center and taste cheese, Chouriço (Portuguese sausage), bifana sandwiches, traditional rices, Port, and Lisbon wines.",
+    icon: "/images/travel-icons/food-tour.png",
+    link: "https://tinyurl.com/yc6cs2c2",
   },
   {
-    title: 'Time Out Market',
-    description: "Taste the iconic pastel de nata pastries and other goodies at this historic market hall.",
-    icon: '/images/travel-icons/time-out-market.png',
-    link: 'https://www.instagram.com/timeoutmarketlisboa/?hl=en'
+    title: "Time Out Market",
+    description:
+      "Taste the iconic pastel de nata pastries and other goodies at this historic market hall.",
+    icon: "/images/travel-icons/time-out-market.png",
+    link: "https://www.instagram.com/timeoutmarketlisboa/?hl=en",
   },
   {
-    title: 'Day Trip to Sintra',
-    description: "Visit the Pena Palace and beautiful surrounding park grounds. It's truly a magical place straight out of a fairytale.",
-    icon: '/images/travel-icons/sintra.png',
-    link: 'https://tinyurl.com/2saxdryt'
+    title: "Day Trip to Sintra",
+    description:
+      "Visit the Pena Palace and beautiful surrounding park grounds. It's truly a magical place straight out of a fairytale.",
+    icon: "/images/travel-icons/sintra.png",
+    link: "https://tinyurl.com/2saxdryt",
   },
   {
-    title: 'A Ginjinha',
-    description: "Walk to A Ginjinha to shoot the sour cherry liquor Lisbon is known for - only 1.55 euros!",
-    icon: '/images/travel-icons/jijinha.png',
-    link: 'https://maps.app.goo.gl/tFev5isk7SzbzZSa6'
+    title: "A Ginjinha",
+    description:
+      "Walk to A Ginjinha to shoot the sour cherry liquor Lisbon is known for - only 1.55 euros!",
+    icon: "/images/travel-icons/jijinha.png",
+    link: "https://maps.app.goo.gl/tFev5isk7SzbzZSa6",
   },
   {
-    title: 'Tejo Bar',
-    description: "Hangout late into the night at Tejo Bar to hear traditional fado music. Quite possibly the smallest bar you'll ever step into!",
-    icon: '/images/travel-icons/guitar.png',
-    link: 'https://maps.app.goo.gl/6aLyEiffVGoXFff57'
+    title: "Tejo Bar",
+    description:
+      "Hangout late into the night at Tejo Bar to hear traditional fado music. Quite possibly the smallest bar you'll ever step into!",
+    icon: "/images/travel-icons/guitar.png",
+    link: "https://maps.app.goo.gl/6aLyEiffVGoXFff57",
   },
   {
-    title: 'Alfama District',
-    description: "Walk around the Alfama district, Lisbon's oldest neighborhood - explore the São Jorge Castle while you're there.",
-    icon: '/images/travel-icons/sao-jorge.png',
-    link: 'https://maps.app.goo.gl/NjHjhtwX8JLwtqSj6'
+    title: "Alfama District",
+    description:
+      "Walk around the Alfama district, Lisbon's oldest neighborhood - explore the São Jorge Castle while you're there.",
+    icon: "/images/travel-icons/sao-jorge.png",
+    link: "https://maps.app.goo.gl/NjHjhtwX8JLwtqSj6",
   },
   {
-    title: 'Iconic Tram 28',
-    description: "Hop on the iconic Tram 28 with a public transport pass for a scenic tour of the city's hills.",
-    icon: '/images/travel-icons/lisbon.png',
-    link: 'https://maps.app.goo.gl/cidmmnAssngt4LZ46'
+    title: "Iconic Tram 28",
+    description:
+      "Hop on the iconic Tram 28 with a public transport pass for a scenic tour of the city's hills.",
+    icon: "/images/travel-icons/lisbon.png",
+    link: "https://maps.app.goo.gl/cidmmnAssngt4LZ46",
   },
   {
-    title: 'Belém Tower',
-    description: "Visit the Belém Tower, an iconic fortress on the Tagus river that served as a point of embarkation for Portuguese explorers.",
-    icon: '/images/travel-icons/belem-tower.png',
-    link: 'https://maps.app.goo.gl/eV1eL5LFwNwPsXHf6'
+    title: "Belém Tower",
+    description:
+      "Visit the Belém Tower, an iconic fortress on the Tagus river that served as a point of embarkation for Portuguese explorers.",
+    icon: "/images/travel-icons/belem-tower.png",
+    link: "https://maps.app.goo.gl/eV1eL5LFwNwPsXHf6",
   },
   {
-    title: 'Day Trip to Cascais',
-    description: "A 30 min drive from Lisbon. It's a laid-back coastal town with great beaches, good food, and easy access to scenic coastal walks.",
-    icon: '/images/travel-icons/waves.png',
-    link: 'https://maps.app.goo.gl/hYUiysxtgpffCvyv8'
+    title: "Day Trip to Cascais",
+    description:
+      "A 30 min drive from Lisbon. It's a laid-back coastal town with great beaches, good food, and easy access to scenic coastal walks. Check out Praia do Guincho, a beautiful beach with good surfing or windsurfing waves!",
+    icon: "/images/travel-icons/waves.png",
+    link: "https://maps.app.goo.gl/hYUiysxtgpffCvyv8",
   },
   {
-    title: 'Day Trip to Obidos',
-    description: "A 1 hour drive from Lisbon. A fairy tale medieval town with charming cobblestone streets, historic castle, and unique local treats",
-    icon: '/images/travel-icons/bouginvillea.png',
-    link: 'https://maps.app.goo.gl/pctcm4JWz7TWvAob7'
-  }
+    title: "Cape Roca",
+    description:
+      "The most western point of Europe! Scenic view of the ocean with iconic lighthouse.",
+    icon: "/images/travel-icons/cabo-da-roca.png",
+    link: "https://maps.app.goo.gl/zNBaDyyLeHBL24BW8",
+  },
+  {
+    title: "Day Trip to Obidos",
+    description:
+      "A 1 hour drive from Lisbon. A fairy tale medieval town with charming cobblestone streets, historic castle, and unique local treats",
+    icon: "/images/travel-icons/bouginvillea.png",
+    link: "https://maps.app.goo.gl/pctcm4JWz7TWvAob7",
+  },
 ];
 
 export const TravelSection: React.FC = () => {
   return (
-    <section id="travel" className="w-full py-24 px-4 md:px-8 bg-wedding-paper text-[#3E2723]">
+    <section
+      id="travel"
+      className="w-full py-24 px-4 md:px-8 bg-wedding-paper text-[#3E2723]"
+    >
       <div className="mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12">
         {/* Left Side: Map */}
-        <div
-          className="relative lg:sticky top-0 w-full h-screen"
-        >
+        <div className="relative lg:sticky top-0 w-full h-screen">
           <Image
             src="/images/map.png"
             alt="Travel Map"
@@ -87,11 +105,16 @@ export const TravelSection: React.FC = () => {
 
         {/* Right Side: Things to Do */}
         <div className="flex flex-col gap-8">
-          <h2 className="font-script text-wedding-green text-6xl lowercase mb-8 text-center">While you're there</h2>
+          <h2 className="font-script text-wedding-green text-6xl lowercase mb-8 text-center">
+            While you're there
+          </h2>
 
           <div className="grid grid-cols-1 gap-x-8 gap-y-12">
             {THINGS_TO_DO.map((item, idx) => (
-              <div key={idx} className="flex lg:flex-row flex-col group items-center lg:items-start">
+              <div
+                key={idx}
+                className="flex lg:flex-row flex-col group items-center lg:items-start"
+              >
                 <div className="relative w-44 h-44 mb-2 transition-transform duration-300 group-hover:scale-110 shrink-0">
                   <Image
                     src={item.icon}
@@ -104,23 +127,24 @@ export const TravelSection: React.FC = () => {
                 <div>
                   <h3 className="font-mono text-xl text-wedding-brown mb-3 flex gap-2">
                     {item.link ? (
-                      <a href={item.link} target="_blank" rel="noopener noreferrer" className="hover:underline hover:text-wedding-brown transition-colors decoration-1 underline-offset-4 flex items-center gap-2">
+                      <a
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:underline hover:text-wedding-brown transition-colors decoration-1 underline-offset-4 flex items-center gap-2"
+                      >
                         {item.title} <ExternalLink size={16} />
                       </a>
                     ) : (
                       item.title
                     )}
                   </h3>
-                  <p className="font-mono text-lg">
-                    {item.description}
-                  </p>
+                  <p className="font-mono text-lg">{item.description}</p>
                 </div>
               </div>
             ))}
           </div>
-
         </div>
-
       </div>
     </section>
   );
